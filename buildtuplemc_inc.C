fb80f09e219ed1f68f09c35ab2d8dda924094ad5
Stanislav <istaslis@gmail.com>
2016-01-15 12:52:43 +0100

    Inclusive ntuple for PbPb/pp qcd/bjet

---
+++ buildtuplemc_inc.C
@@ -0,0 +1,180 @@
+/* buildtuplemc.C - makes lightweight ntuples from HiForest files
+ * change samplesfolder if necessary and subfoldernames, pthats
+ * change jettree if you want to use different jet algo
+ * nonoverlapping = true means events from pt,hat_bin[i-1] do not overlap 
+ *                  with pt,hat_bin[i]. This reduces statistics by 10-20%, 
+ *                  but allows to check if large weights corrupt errors at high pt
+ */
+
+TString samplesfolder, outputfilename, jettree;
+vector<TString> subfoldernames;
+vector<int> pthats;
+
+
+void Init(TString colType, TString mcType)
+{
+  TString outputfolder = "/data_CMS/cms/lisniak/bjet2015/";
+  if (colType=="PbPb" && mcType=="qcd") {
+    samplesfolder="/data_CMS/cms/mnguyen/bJet2015/mc/PbPb";
+    subfoldernames={"qcd30","qcd50","qcd80","qcd120","qcd170"};
+    pthats = {           30,     50,     80,     120,     170};
+    outputfilename = outputfolder+"mcPbPbqcd_inc.root";
+    jettree = "akPu4PFJetAnalyzer/t";
+  }
+
+  else  if (colType=="PbPb" && mcType=="bjet") {
+    samplesfolder="/data_CMS/cms/mnguyen/bJet2015/mc/PbPb";
+    subfoldernames={"b30","b50","b80","b120","b170"};
+    pthats = {           30,     50,     80,     120,     170};
+    outputfilename = outputfolder+"mcPbPbbjet_inc.root";
+    jettree = "akPu4PFJetAnalyzer/t";
+  }
+
+  else  if (colType=="pp" && mcType=="qcd") {
+    samplesfolder="/data_CMS/cms/mnguyen/bJet2015/mc/pp";
+    subfoldernames={"qcd30","qcd50","qcd80","qcd120","qcd170"}; //just a typo in the foldername
+    pthats = {           30,     50,     80,     120,     220};
+    outputfilename = outputfolder+"mcppqcd_inc.root";
+    jettree = "ak4PFJetAnalyzer/t";
+  } 
+
+  else  if (colType=="pp" && mcType=="bjet") {
+    samplesfolder="/data_CMS/cms/mnguyen/bJet2015/mc/pp";
+    subfoldernames={"b30","b50","b80","b120","b170"};
+    pthats = {           30,   50,   80,   120,   170};
+    outputfilename = outputfolder+"mcppbjet_inc.root";
+    jettree = "ak4PFJetAnalyzer/t";
+  }
+  else cout<<"Unknown type of collision-mc. Use PbPb/pp-qcd/bjet"<<endl;
+  
+}
+
+
+//non-overlapping pt,hat bins loose 10-20% of statistics, but don't have large weights issue
+bool nonoverlapping = false;
+
+
+int getind (float pthat)
+{
+  if (pthat<pthats[0])
+    cout<<"pthat "<<pthat<<" is less than minimum "<<pthats[0];
+
+  for (int i=1;i<pthats.size();i++) 
+    if (pthat<pthats[i]) return i-1;
+
+  return pthats.size()-1;
+  
+}
+
+TTree *GetTree(TFile *f, TString treename)
+{
+  TTree *t = (TTree *)f->Get(treename);
+  //PbPb bjet pthat120 has only unsubtracted jets!!!!! 
+  //TODO: figure out
+  if (t==0) t = (TTree *)f->Get("ak4PFJetAnalyzer/t");
+  return t;
+}
+
+void buildtuplemc_inc(TString colType="PbPb", TString mcType="qcd")
+{
+  Init(colType, mcType);
+
+  int bins = pthats.size();
+
+  vector<double> weights (bins);
+  vector<double> eventsCurbin(pthats.size());
+  vector<double> eventsNextbin(pthats.size());
+  vector<double> abovethresh(bins);
+  weights[0] = 1;
+
+  cout<<"Calculating weights"<<endl;
+  cout<<" pthat "<<pthats[0]<<"\t"<<flush;
+  for (int i=1;i<pthats.size();i++) {
+    cout<<" pthat "<<pthats[i]<<"\t"<<flush;
+    TFile *f0 = new TFile(Form("%s/%s/merged_HiForestAOD.root",samplesfolder.Data(),subfoldernames[i].Data()));
+    TTree *t0 = GetTree(f0,jettree); 
+    double x0 = t0->GetEntries(Form("pthat>%d",pthats[i]));
+
+    
+    int minoverlap = nonoverlapping ? i-1 : 0; 
+    double x1=0;
+    for (int j=minoverlap;j<i;j++) {
+      TFile *f1 = new TFile(Form("%s/%s/merged_HiForestAOD.root",samplesfolder.Data(),subfoldernames[j].Data()));
+      TTree *t1 = GetTree(f1, jettree);
+      x1+=t1->GetEntries(Form("pthat>%d",pthats[i]));
+      f1->Close();
+    }
+    //    cout<<pthats[i]<<" "<<x1<<" "<<x0<<" "<<weights[i-1]<<endl;
+    weights[i] = nonoverlapping ? weights[i-1]*x1/x0 : weights[i-1]*x1/(x0+x1);
+    
+    f0->Close();
+  }
+  double sumw = 0;
+  for (auto w:weights) sumw+=w;
+  for (int i=0;i<weights.size();i++) weights[i]/=sumw;
+
+  cout<<endl<<"Weights : "<<endl;
+  for (auto w:weights) cout<<w<<"\t";
+  cout<<endl;
+
+  int totentries = 0;
+
+  //now fill histos
+  TFile *fout = new TFile(outputfilename,"recreate");
+  TNtuple *nt = new TNtuple("nt","nt","pthat:weight:genpt:jtpt:jtphi:jteta:discr_csvSimple:refparton_flavorForB");
+  
+  for (int i=0;i<pthats.size();i++) {
+    TString filename = TString::Format("%s/%s/merged_HiForestAOD.root",samplesfolder.Data(),subfoldernames[i].Data());
+    cout<<endl<<"Processing file "<<filename<<endl;
+
+    TFile *f = new TFile(filename);
+    //PbPb bjet pthat120 has only unsubtracted jets!!!!
+    TString treename = f->Get(jettree) != 0 ? jettree : "ak4PFJetAnalyzer";
+    TTreeReader reader(jettree,f);
+    TTreeReaderValue<float> pthat(reader, "pthat");
+    TTreeReaderValue<int> nref(reader, "nref");
+    TTreeReaderArray<float> genpt(reader, "genpt");
+    TTreeReaderArray<float> jtpt(reader, "jtpt");
+    TTreeReaderArray<float> jteta(reader, "jteta");
+    TTreeReaderArray<float> jtphi(reader, "jtphi");
+    TTreeReaderArray<float> discr_csvSimple(reader, "discr_csvSimple");
+    TTreeReaderArray<int> refparton_flavorForB(reader, "refparton_flavorForB");
+    
+    int nev = reader.GetEntries(true);
+    totentries+=nev;
+    int onep = nev/100;
+    int evCounter = 0;
+    TTimeStamp t0;
+
+    while (reader.Next()) {
+      evCounter++;
+      if (evCounter%onep==0) {
+	std::cout << std::fixed;
+	TTimeStamp t1; 
+	cout<<" \r"<<evCounter/onep<<"%   "<<" total time "<<(int)round((t1-t0)*nev/(evCounter+.1))<<" s "<<flush;
+      }
+
+      int ind[2];
+      bool foundLJ=false, foundSJ = false;
+      
+      for (int j=0;j<*nref;j++) {
+
+      vector<float> v;
+      v = {*pthat, (float)weights[getind(*pthat)],
+	   genpt[j], jtpt[j], jtphi[j], jteta[j], discr_csvSimple[j],
+	   (float)refparton_flavorForB[j]};
+      
+      nt->Fill(&v[0]);
+      }
+    }
+    f->Close();
+  }
+  
+  fout->cd();
+  nt->Write();
+  fout->Close();
+
+  cout<<endl;
+  cout<<"Total input entries "<<totentries<<endl;
+
+  }
---
+++ buildtuples.sh
@@ -0,0 +1,4 @@
+root -l -q buildtuplemc_inc.C\(\"PbPb\",\"qcd\"\) &
+root -l -q buildtuplemc_inc.C\(\"pp\",\"qcd\"\) &
+root -l -q buildtuplemc_inc.C\(\"PbPb\",\"bjet\"\) &
+root -l -q buildtuplemc_inc.C\(\"pp\",\"bjet\"\) &
