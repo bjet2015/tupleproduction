110b081d74538609ea66ce2359634e9a79f79250
Stanislav <istaslis@gmail.com>
2016-01-21 11:37:20 +0100

    updated sh

--- buildtuples.sh
+++ buildtuples.sh
@@ -1,9 +1,6 @@
-root -l -q buildtuplemc_inc.C\(\"PbPb\",\"qcd\"\) &
-root -l -q buildtuplemc_inc.C\(\"pp\",\"qcd\"\) &
-root -l -q buildtuplemc_inc.C\(\"PbPb\",\"bjet\"\) &
-root -l -q buildtuplemc_inc.C\(\"pp\",\"bjet\"\) &
+#root -l -q buildtuplemc.C\(\"PbPb\",\"qcd\"\) &
+root -l -q buildtuplemc.C\(\"pp\",\"qcd\"\) &
+#root -l -q buildtuplemc.C\(\"PbPb\",\"bjet\"\) &
+#root -l -q buildtuplemc.C\(\"pp\",\"bjet\"\) &
 
-root -l -q buildtuplemc_dj.C\(\"PbPb\",\"qcd\"\) &
-root -l -q buildtuplemc_dj.C\(\"pp\",\"qcd\"\) &
-root -l -q buildtuplemc_dj.C\(\"PbPb\",\"bjet\"\) &
-root -l -q buildtuplemc_dj.C\(\"pp\",\"bjet\"\) &
+root -l -q buildtupledata.C\(\"pp_PFLowPt\",\"ak4PFJetAnalyzer\"\) &
